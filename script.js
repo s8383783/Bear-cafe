6e75a2c8be87123641334346737a20528b8f3c2e
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-06-29 06:43:50 -0700

    updates

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 'use strict'
-let name = prompt('What is your name?');
+function getName() 
+{let name = prompt('What is your name?');
 document.write('Welcome ' + name + '     '+ ',');
-let email = prompt('Please enter your email. ');
-let confirmation = prompt(' Is this your email? ' +' ' + email);
-if (confirmation === 'Y') {
-    document.write('thank you for confirming your email');
 };
-if (confirmation === 'N') {
-    document.write(' Please refresh page. ');
+
+function getEmail(){
+    let email = prompt('Please enter your email. ');
+    let confirmation = prompt(' Is this your email? ' +' ' + email);
+    if (confirmation === 'Y') {
+        document.write('thank you for confirming your email');
+    };
+    if (confirmation === 'N') {
+        document.write(' Please refresh page. ');
 }
 
+
+}
+
+document.write(getName)
+document.write(getEmail)
