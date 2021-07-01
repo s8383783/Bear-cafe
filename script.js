0d04e6dfca8843f99b9d064ea303148c2727e70e
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-06-30 19:22:28 -0700

    bears

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ document.write('Welcome ' + name + '     '+ ',');
         document.write(' Please refresh page. ');
 }
 
-
-
-
+let rating = prompt('what is your rating for this restaurant?');
+for (let i = 0; i < rating; i++) {
+    document.write('<img id= "stars" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/1200px-Five-pointed_star.svg.png">');
+      } 
+let bears = prompt('Do you like bears?')
+while (bears != 'yes') {
+   bears = prompt('TRY AGAIN. DO YOU LIKE BEARS?') 
+}
