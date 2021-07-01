00d26dfbd92c24e8d31edd20df001f5214081cdf
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-06-30 19:24:37 -0700

    bears

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let name = prompt('What is your name?');
 document.write('Welcome ' + name + '     '+ ',');
     let email = prompt('Please enter your email. ');
     let confirmation = prompt(' Is this your email? ' +' ' + email);
-    if (confirmation === 'Y') {
+    if (confirmation === 'yes') {
         document.write('thank you for confirming your email');
     };
-    if (confirmation === 'N') {
+    if (confirmation === 'no') {
         document.write(' Please refresh page. ');
 }
 
